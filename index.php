1e57b150586d8f226803fe125b607d3b73edd333
tim-bm <tim.cbt@gmail.com>
2016-10-01 15:00:45 +1000

    test from zend

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $method=$_SERVER['REQUEST_METHOD'];
-//aaaaa
+
 header('Content-type: application/json');
 if ($method=='GET'){
-	$json=array("status"=>1,"msg"=>"a GET response from index");
+	$json=array("status"=>1,"msg"=>"a GET response from index11111111111111");
 	echo json_encode($json);
 }
