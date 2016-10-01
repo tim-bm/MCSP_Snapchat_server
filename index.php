20dab06094901370a71b236a9ffc3fbf8f304859
tim-bm <tim.cbt@gmail.com>
2016-10-01 12:40:57 +1000

    Get simple
    Change-Id: I83b01bbbad27e8dd740194ba21615ceb4914cba0

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-print "Test for Snapchat server 222222";
+
+$method=$_SERVER['REQUEST_METHOD'];
+
+header('Content-type: application/json');
+if ($method=='GET'){
+	$json=array("status"=>1,"msg"=>"a GET response from index");
+	echo json_encode($json);
+}
